74d2a057f0cec38be5f1df2de74f4cfb533562db
robeth <robethrahmatullah@gmail.com>
2016-06-09 00:44:38 +0700

    Simplify setup script & create backup instead of delete

--- setup.sh
+++ setup.sh
@@ -9,26 +9,23 @@ TMUX_DIR="$HOME/.tmux"
 VIM_CONF_FILE="$HOME/.vimrc"
 VIM_DIR="$HOME/.vim"
 
-if [ -e $TMUX_CONF_FILE ]
-then
-  rm $TMUX_CONF_FILE
-fi
-if [ -d $TMUX_DIR ]
-then
-  rm -rf $TMUX_DIR
-fi
+function backup {
+  if [ -e $1 ]
+  then
+    mv -T $1 "$1.backup"
+  fi
+}
+
+backup $TMUX_CONF_FILE
+backup $TMUX_DIR
+
 ln -s "$DOTFILES_DIR/tmux/tmux.conf" $TMUX_CONF_FILE
 ln -s "$DOTFILES_DIR/tmux/tmux" $TMUX_DIR
 
 # Setup Vim symbolic links
-if [ -e $VIM_CONF_FILE ]
-then
-  rm $VIM_CONF_FILE
-fi
-if [ -d $VIM_DIR ]
-then
-  rm -rf $VIM_DIR
-fi
+backup $VIM_CONF_FILE
+backup $VIM_DIR
+
 ln -s "$DOTFILES_DIR/vim/vimrc" $VIM_CONF_FILE
 ln -s "$DOTFILES_DIR/vim/vim" $VIM_DIR
 
