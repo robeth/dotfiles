29f22d4a40606c99732f330680d8400cb3a1cd55
robeth <robethrahmatullah@gmail.com>
2016-06-09 00:14:00 +0700

    Add comment & citation

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Setup tmux symbolic links
 
+# Get script directory: http://stackoverflow.com/a/246128
 DOTFILES_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 
 TMUX_CONF_FILE="$HOME/.tmux.conf"
